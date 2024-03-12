b2fa3dcc1a3400fb96c6cfce9904b21aaebc2b81
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-03-12 19:20:44 +0100

    copyright: add header to recently-modified files

--- src/main/java/it/polimi/ingsw/am07/utils/Matrix.java
+++ src/main/java/it/polimi/ingsw/am07/utils/Matrix.java
@@ -1,3 +1,26 @@
+/*
+ * Codex Naturalis - Final Assignment for the Software Engineering Course
+ * Copyright (C) 2024 Andrea Biasion Somaschini, Roberto Alessandro Bertolini, Omar Chaabani, Gabriele Corti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Please note that the GNU General Public License applies only to the
+ * files that contain this license header. Other files within the project, such
+ * as assets and images, are property of the original owners and may be
+ * subject to different copyright terms.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.polimi.ingsw.am07.utils;
 
 import java.util.ArrayList;
