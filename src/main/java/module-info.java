1530220d659081a83c4aee8c93a206ef1f8946d6
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-03-22 19:42:00 +0100

    fix: delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module it.polimi.ingsw.am07 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.squareup.moshi;
-
-    requires org.controlsfx.controls;
-
-    exports it.polimi.ingsw.am07;
-}
