cacc2e659ff2ce5440aebb055f50a513926a5fc3
Omar Chaabani <omarchaabani22@gmail.com>
2024-04-04 00:22:28 +0200

    test: add SideFrontResTest

---
+++ src/test/java/it/polimi/ingsw/am07/model/game/side/SideFrontResTest.java
@@ -0,0 +1,45 @@
+/*
+ * Codex Naturalis - Final Assignment for the Software Engineering Course
+ * Copyright (C) 2024 Andrea Biasion Somaschini, Roberto Alessandro Bertolini, Omar Chaabani, Gabriele Corti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Please note that the GNU General Public License applies only to the
+ * files that contain this license header. Other files within the project, such
+ * as assets and images, are property of the original owners and may be
+ * subject to different copyright terms.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.polimi.ingsw.am07.model.game.side;
+
+import it.polimi.ingsw.am07.model.game.Symbol;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SideFrontResTest {
+
+    @Test
+    void calculateAssociatedScore() {
+
+        SideFrontRes side = new SideFrontRes(1,null,null,Symbol.NONE,2);
+
+        assertEquals(2, side.calculateAssociatedScore());
+
+        side = new SideFrontRes(2, null, null, Symbol.BLUE, 4);
+
+        assertEquals(4,side.calculateAssociatedScore());
+
+    }
+}
