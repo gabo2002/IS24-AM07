aa41d944fcedfcf003e0ec27585977c42df2a9f0
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-04-04 14:09:36 +0200

    feat: implement AssetsRegistry and GameResources to hold any game asset

---
+++ src/main/java/it/polimi/ingsw/am07/model/game/GameResources.java
@@ -0,0 +1,117 @@
+/*
+ * Codex Naturalis - Final Assignment for the Software Engineering Course
+ * Copyright (C) 2024 Andrea Biasion Somaschini, Roberto Alessandro Bertolini, Omar Chaabani, Gabriele Corti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Please note that the GNU General Public License applies only to the
+ * files that contain this license header. Other files within the project, such
+ * as assets and images, are property of the original owners and may be
+ * subject to different copyright terms.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.polimi.ingsw.am07.model.game;
+
+import it.polimi.ingsw.am07.model.game.card.GameCard;
+import it.polimi.ingsw.am07.model.game.card.ObjectiveCard;
+import it.polimi.ingsw.am07.utils.AssetsRegistry;
+import it.polimi.ingsw.am07.utils.json.GameDataJsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * GameResources is a singleton that contains the game resources.
+ */
+public class GameResources {
+
+    private static GameResources instance;
+
+    private final List<GameCard> goldCards;
+
+    private final List<GameCard> resourceCards;
+
+    private final List<ObjectiveCard> objectiveCards;
+
+    public static int CARDS_COUNT = AssetsRegistry.CARDS_COUNT;
+
+    public static int OBJECTIVES_COUNT = AssetsRegistry.OBJECTIVES_COUNT;
+
+    private GameResources() {
+        goldCards = new ArrayList<>();
+        resourceCards = new ArrayList<>();
+        objectiveCards = new ArrayList<>();
+    }
+
+    /**
+     * Returns the singleton instance of the class.
+     *
+     * @return
+     */
+    public static GameResources getInstance() {
+        if (instance == null) {
+            instance = new GameResources();
+            instance.initializeCards();
+        }
+        return instance;
+    }
+
+    /**
+     * Initializes the cards from the backing JSON.
+     */
+    private void initializeCards() {
+        GameDataJsonParser<GameCard> gameCardParser = new GameDataJsonParser<>(GameCard.class);
+
+        String cardsJson = AssetsRegistry.getCardsJson();
+
+        List<GameCard> cards = gameCardParser.listFromJson(cardsJson);
+
+        resourceCards.addAll(cards.subList(0, AssetsRegistry.CARDS_COUNT));
+        goldCards.addAll(cards.subList(AssetsRegistry.CARDS_COUNT, cards.size()));
+
+        GameDataJsonParser<ObjectiveCard> objectiveCardParser = new GameDataJsonParser<>(ObjectiveCard.class);
+
+        String objectivesJson = AssetsRegistry.getObjectivesJson();
+
+        objectiveCards.addAll(objectiveCardParser.listFromJson(objectivesJson));
+    }
+
+    /**
+     * Returns the gold cards.
+     *
+     * @return the gold cards
+     */
+    public List<GameCard> getGoldCards() {
+        return goldCards;
+    }
+
+    /**
+     * Returns the resource cards.
+     *
+     * @return the resource cards
+     */
+    public List<GameCard> getResourceCards() {
+        return resourceCards;
+    }
+
+    /**
+     * Returns the objective cards.
+     *
+     * @return the objective cards
+     */
+    public List<ObjectiveCard> getObjectiveCards() {
+        return objectiveCards;
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/am07/utils/AssetsRegistry.java
@@ -0,0 +1,73 @@
+/*
+ * Codex Naturalis - Final Assignment for the Software Engineering Course
+ * Copyright (C) 2024 Andrea Biasion Somaschini, Roberto Alessandro Bertolini, Omar Chaabani, Gabriele Corti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Please note that the GNU General Public License applies only to the
+ * files that contain this license header. Other files within the project, such
+ * as assets and images, are property of the original owners and may be
+ * subject to different copyright terms.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.polimi.ingsw.am07.utils;
+
+import it.polimi.ingsw.am07.Application;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class AssetsRegistry {
+
+    private static String CARDS_JSON = "cards.json";
+
+    private static String OBJECTIVES_JSON = "objectives.json";
+
+    public static int CARDS_COUNT = 40;
+
+    public static int OBJECTIVES_COUNT = 0;
+
+    public static String getCardsJson() {
+        String json = null;
+
+        try (InputStream inputStream = Application.class.getResourceAsStream(CARDS_JSON)) {
+            if (inputStream == null) {
+                throw new RuntimeException("Failed to load card asset");
+            }
+            json = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read card asset");
+        }
+
+        return json;
+    }
+
+    public static String getObjectivesJson() {
+        String json = null;
+
+        try (InputStream inputStream = Application.class.getResourceAsStream(OBJECTIVES_JSON)) {
+            if (inputStream == null) {
+                throw new RuntimeException("Failed to load objective asset");
+            }
+            json = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read objective asset");
+        }
+
+        return json;
+    }
+
+}
