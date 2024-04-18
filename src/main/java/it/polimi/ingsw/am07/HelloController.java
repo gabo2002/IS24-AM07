a8481e439fa477e38d66a675215ab4031d2b3d70
Omar Chaabani <omarchaabani22@gmail.com>
2024-04-18 14:48:47 +0200

    feat: add welcome-view.fxml (for now styled like this) + welcome.css

--- src/main/java/it/polimi/ingsw/am07/HelloController.java
+++ src/main/java/it/polimi/ingsw/am07/HelloController.java
@@ -2,13 +2,17 @@ package it.polimi.ingsw.am07;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField nicknameField;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome " + nicknameField.getText());
     }
 }
