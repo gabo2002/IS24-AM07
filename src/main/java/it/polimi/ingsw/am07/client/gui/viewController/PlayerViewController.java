2ea2a19628cb7466cfc52e1bc514016f1b983b6c
Andrea Biasion <andrea.biasion@mail.polimi.it>
2024-06-17 14:44:48 +0200

    feat: add deck

--- src/main/java/it/polimi/ingsw/am07/client/gui/viewController/PlayerViewController.java
+++ src/main/java/it/polimi/ingsw/am07/client/gui/viewController/PlayerViewController.java
@@ -26,14 +26,24 @@ package it.polimi.ingsw.am07.client.gui.viewController;
 import it.polimi.ingsw.am07.action.chat.SendMessageAction;
 import it.polimi.ingsw.am07.chat.ChatMessage;
 import it.polimi.ingsw.am07.model.ClientState;
+import it.polimi.ingsw.am07.model.game.Deck;
 import it.polimi.ingsw.am07.model.game.Player;
+import it.polimi.ingsw.am07.model.game.card.GameCard;
 import it.polimi.ingsw.am07.reactive.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +61,12 @@ public class PlayerViewController {
 
     @FXML
     private TextArea chatArea;
+
+    @FXML
+    private HBox resourceDeckContainer;
+
+    @FXML
+    private HBox goldDeckContainer;
 
     private ClientState clientState;
     private Controller controller;
@@ -76,7 +92,7 @@ public class PlayerViewController {
             scoreLabel.setText("Score: " + clientState.getGameModel().getSelf().getPlayerScore());
         }
 
-        //Retrieve the last 20 messages
+        // Retrieve the last 20 messages
         List<ChatMessage> messages = clientState.getGameModel().getSelf().getChat().getMessages();
         List<String> messageRepresentation = new ArrayList<>(messages.size());
         for (ChatMessage message : messages) {
@@ -85,7 +101,77 @@ public class PlayerViewController {
         }
         chatArea.clear();
         chatArea.setText(String.join("\n", messageRepresentation));
+
+        Deck deck = clientState.getGameModel().getDeck();
+        List<GameCard> resourceCards = deck.availableResCards();
+        List<GameCard> goldCards = deck.availableGoldCards();
+
+        updateDeckView(resourceDeckContainer, resourceCards);
+        updateDeckView(goldDeckContainer, goldCards);
     }
+
+    private void updateDeckView(HBox deckContainer, List<GameCard> cards) {
+        deckContainer.getChildren().clear();
+        Image coverImage = imgFrom(cards.getFirst().id(), "back");
+        if (!cards.isEmpty()) {
+            ImageView coverImageView = createImageView(coverImage);
+            deckContainer.getChildren().add(coverImageView);
+
+            for (int i = 1; i < Math.min(3, cards.size()); i++) {
+                Image cardImage = imgFrom(cards.get(i).id(), "front");
+                ImageView imageView = createImageView(cardImage);
+                deckContainer.getChildren().add(imageView);
+            }
+        }
+    }
+
+    private Image imgFrom(int id, String side) {
+        ClassLoader cl = this.getClass().getClassLoader();
+        String item = "it/polimi/ingsw/am07/assets/"+side+"_" + id + ".png";
+        Image img = null;
+        try (InputStream is = cl.getResourceAsStream(item)) {
+            if (is != null) {
+                img = new Image(is);
+            } else {
+                System.err.println("Unable to load image: " + item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return img;
+    }
+
+    private ImageView createImageView(Image image) {
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(150.0);
+        imageView.setFitWidth(200.0);
+        imageView.setPickOnBounds(true);
+        imageView.setPreserveRatio(true);
+
+        return imageView;
+    }
+    private ImageView createImageView(int id, String initialSide) {
+        Image initialImage = imgFrom(id, initialSide);
+        ImageView imageView = new ImageView(initialImage);
+        imageView.setFitHeight(150.0);
+        imageView.setFitWidth(200.0);
+        imageView.setPickOnBounds(true);
+        imageView.setPreserveRatio(true);
+
+        imageView.getProperties().put("currentSide", initialSide);
+
+        imageView.setOnMouseClicked(event -> {
+            String currentSide = (String) imageView.getProperties().get("currentSide");
+            String newSide = currentSide.equals("front") ? "back" : "front";
+            Image newImage = imgFrom(id, newSide);
+            imageView.setImage(newImage);
+            imageView.getProperties().put("currentSide", newSide);
+        });
+
+        return imageView;
+    }
+
+
 
     @FXML
     protected void sendChatMessage() {
@@ -106,5 +192,7 @@ public class PlayerViewController {
 
         controller.execute(action);
     }
+
+
 
 }
