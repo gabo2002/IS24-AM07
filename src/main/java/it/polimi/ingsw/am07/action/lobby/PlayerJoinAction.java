2f5f6884e88483ff9171aabbfaf7c6d0c68f6f8a
Roberto Bertolini <robertoalessandro.bertolini@gmail.com>
2024-06-23 16:22:28 +0200

    style: remove duplicated copyright header

--- src/main/java/it/polimi/ingsw/am07/action/lobby/PlayerJoinAction.java
+++ src/main/java/it/polimi/ingsw/am07/action/lobby/PlayerJoinAction.java
@@ -23,29 +23,6 @@
 
 package it.polimi.ingsw.am07.action.lobby;
 
-/*
- * Codex Naturalis - Final Assignment for the Software Engineering Course
- * Copyright (C) 2024 Andrea Biasion Somaschini, Roberto Alessandro Bertolini, Omar Chaabani, Gabriele Corti
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * Please note that the GNU General Public License applies only to the
- * files that contain this license header. Other files within the project, such
- * as assets and images, are property of the original owners and may be
- * subject to different copyright terms.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 
 import it.polimi.ingsw.am07.action.PlayerAction;
 import it.polimi.ingsw.am07.model.ClientState;
@@ -66,10 +43,10 @@ public class PlayerJoinAction extends PlayerAction {
     /**
      * The player join for CLI
      *
-     * @param playerNickname
-     * @param identity
-     * @param lobbyId
-     * @param color
+     * @param playerNickname    the player's nickname
+     * @param identity          the player's identity
+     * @param lobbyId           the lobby's id
+     * @param color             the player's pawn color
      */
     public PlayerJoinAction(String playerNickname, String identity, UUID lobbyId, Pawn color) {
         super(playerNickname, identity);
